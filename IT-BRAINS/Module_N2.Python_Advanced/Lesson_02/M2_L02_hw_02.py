f96ed96bd4505c2a910d325366a48e9789a68353
Leonid Kadantsev <leonid.cordon@gmail.com>
2023-08-02 16:00:56 +0300

    Module 2, Lecture 2, Homework 2

---
+++ IT-BRAINS/Module_N2.Python_Advanced/Lesson_02/M2_L02_hw_02.py
@@ -0,0 +1,32 @@
+# Module 2, Lecture 2, Homework 2
+
+# Create a function that accepts two numbers from the user йand handles an error (и обрабатывает ошибку)
+# if the input is not a number.
+
+
+def lk_get_two_numbers():
+    """
+    This function prompts the user to enter two numbers and returns them as a tuple.
+    If the user enters invalid input (i.e. not a number), the function will print an error message
+    and continue to prompt the user until valid input is entered.
+    """
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+
+    return num1, num2
+
+
+# Example usage of the lk_get_two_numbers function
+try:
+    num01, num02 = lk_get_two_numbers()
+    print("Numbers entered:", num01, "and", num02)
+    print(f"The sum of {num01} and {num02} is {num01 + num02}")
+except KeyboardInterrupt:
+    # Handle the case where the user cancels the operation
+    print("\nOperation cancelled by the user.")
+
